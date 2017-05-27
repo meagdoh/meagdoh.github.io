53fd458a148a86cd2e5f3e0f95b0c1912e8fe50c
meagdoh <meagdoherty@gmail.com>
2017-05-27 12:37:57 -0400

    Update layout

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+$(function () {
+  count = 0;
+  wordsArray = ["yogi", "bear"];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(300, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(300);
+    });
+  }, 2000);
+});
+
+
+
+});
+// // Variables
