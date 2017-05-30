c9454b790b2b1115b2fd8da24a1df09dc68102bc
meagdoh <meagdoherty@gmail.com>
2017-05-30 09:29:37 -0400

    Adjust font size and formatting

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-
-
-$(function () {
-  count = 0;
-  wordsArray = ["yogi", "bear"];
-  setInterval(function () {
-    count++;
-    $("#word").fadeOut(300, function () {
-      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(300);
-    });
-  }, 2000);
-});
-
-
-
-});
-// // Variables
+// $(document).ready(function(){
+//
+//
+// $(function () {
+//   count = 0;
+//   wordsArray = ["yogi", "bear"];
+//   setInterval(function () {
+//     count++;
+//     $("#word").fadeOut(300, function () {
+//       $(this).text(wordsArray[count % wordsArray.length]).fadeIn(300);
+//     });
+//   }, 2000);
+// });
+//
+//
+//
+// });
+// // // Variables
